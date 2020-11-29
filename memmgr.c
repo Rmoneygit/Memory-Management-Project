44e42ec535f0b7a0e09ddd9439c923a919cbada1
Ryan <rmoneymail5@gmail.com>
2020-11-28 20:19:55 -0800

    TLB and stats implemented.

--- memmgr.c
+++ memmgr.c
@@ -2,11 +2,9 @@
 //  memmgr.c
 //  memmgr
 //
-//  Created by William McCarthy on 17/11/20.
-//  Copyright © 2020 William McCarthy. All rights reserved.
-//
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
@@ -17,7 +15,9 @@
 #define FRAME_SIZE  256
 
 unsigned page_table[256];
-int memoryFull = 0;
+int TLBsize = 0;
+unsigned TLB[16][2];
+int memoryMax = 0;
 char memory[256][256];
 
 //-------------------------------------------------------------------
@@ -33,11 +33,11 @@ void pageFault(unsigned page, char buf[]){
   fread(buf, BUFLEN, 1, fback);
 
   for(int i=0; i<256; i++){
-    memory[memoryFull][i] = buf[i];
+    memory[memoryMax][i] = buf[i];
   }
 
-  page_table[page] = memoryFull;
-  memoryFull++;
+  page_table[page] = memoryMax;
+  memoryMax++;
 
   fclose(fback);
 }
@@ -61,12 +61,12 @@ int main(int argc, const char* argv[]) {
   if (fcorr == NULL) { fprintf(stderr, "Could not open file: 'correct.txt'\n");  exit(FILE_ERROR);  }
 
   char buf[BUFLEN];
-  unsigned   page, offset, physical_add, frame = 0;
+  unsigned   page, offset, physical_add, address_value, frame = 0;
   unsigned   logic_add;                  // read from file address.txt
   unsigned   virt_add, phys_add, value;  // read from file correct.txt
+  unsigned TLBhitCount = 0;
+  unsigned pageFaultCount = 0;
   memset(page_table, -1, sizeof(page_table));
-
-  printf("ONLY READ FIRST 20 entries -- TODO: change to read all entries\n\n");
 
   while (frame < 1000) {
 
@@ -77,22 +77,58 @@ int main(int argc, const char* argv[]) {
     page   = getpage(logic_add);
     offset = getoffset(logic_add);
 
-    // If the page number is not in the page table
-    if(page_table[page] == -1){
-      pageFault(page, buf);
+    bool TLBhit = false;
+
+    // Try to find the page in the TLB
+    for(int i = 0; i < TLBsize; i++){
+      if(TLB[i][0] == page){
+        TLBhit = true;
+        address_value = memory[TLB[i][1]][offset];
+        physical_add = TLB[i][1]*256 + offset;
+        TLBhitCount++;
+        break;
+      }
+    }
+
+    // Do this stuff if we didn't find it in the TLB
+    if(!TLBhit){
+      // If the page number is not in the page table
+      if(page_table[page] == -1){
+        pageFault(page, buf);
+        pageFaultCount++;
+      }
+
+      // If the TLB still has room, append new entry
+      if(TLBsize != 16){
+        TLB[TLBsize][0] = page;
+        TLB[TLBsize][1] = page_table[page];
+        TLBsize++;        
+      }
+      else{ // If its full, overwrite the oldest entry. (FIFO)
+        for(int i=0; i<15; i++){
+          TLB[i][0] = TLB[i+1][0];
+          TLB[i][1] = TLB[i+1][1];
+        }
+        TLB[15][0] = page;
+        TLB[15][1] = page_table[page];
+      }
+
     }
     
     physical_add = page_table[page] * FRAME_SIZE + offset; 
     assert(physical_add == phys_add);
+
+    address_value = memory[page_table[page]][offset];
+    assert(address_value == value);
     
-    // todo: read BINARY_STORE and confirm value matches read value from correct.txt
-    
-    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -- passed\n", logic_add, page, offset, physical_add);
+    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u (value: %4d) -- passed\n", logic_add, page, offset, physical_add, value);
     frame++;
     if (frame % 5 == 0) { printf("\n"); }
   }
   fclose(fcorr);
   fclose(fadd);
+  printf("Page fault rate: %.2f %%\n", (1.0*pageFaultCount)/(1000.0)*100.0);
+  printf("TLB hit rate: %.2f %%\n", (1.0*TLBhitCount) / (1000.0)*100.0);
   printf("\n\t\t...done.\n");
   return 0;
 }
